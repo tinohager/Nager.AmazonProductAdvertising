f5d9f3ed2411df38d54e078fb9579e95d79045c9
Tino Hager <tino.hager@nager.at>
2016-12-31 13:19:32 +0100

    add country switch to install script

--- Nager.AmazonProductAdvertising/Install.ps1
+++ Nager.AmazonProductAdvertising/Install.ps1
@@ -1,3 +1,25 @@
 param($installPath, $toolsPath, $package, $project)
 
-Start-Process -FilePath "https://www.amazon.de/?tag=nagerat-21"
+$lcid = (Get-Culture).LCID
+$url = "https://www.amazon.com/?tag=nagerat-20"
+
+switch ($lcid) 
+{
+	#Germany
+	1031
+	{
+		$url = "https://www.amazon.de/?tag=nagerat-21"
+	}
+	#Austria
+	3079
+	{
+		$url = "https://www.amazon.de/?tag=nagerat-21"
+	}
+	#Switzerland
+	2055
+	{
+		$url = "https://www.amazon.de/?tag=nagerat-21"
+	}
+}
+
+Start-Process -FilePath $url
