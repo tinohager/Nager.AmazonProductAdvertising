18a4ae7bd97ddf6960019eb1303ea102fbae6ced
ryn0 <admin@bootbaron.com>
2017-11-22 16:37:47 -0500

    add back xml attributes which enable correct parsing

--- Nager.AmazonProductAdvertising/Model/Tracks.cs
+++ Nager.AmazonProductAdvertising/Model/Tracks.cs
@@ -1,5 +1,8 @@
-﻿namespace Nager.AmazonProductAdvertising.Model
+﻿using System.Xml.Serialization;
+
+namespace Nager.AmazonProductAdvertising.Model
 {
+    [XmlRoot(Namespace = "", IsNullable = false)]
     public class Tracks
     {
         public TracksDisc Disc { get; set; }
@@ -7,15 +10,19 @@
 
     public class TracksDisc
     {
+        [XmlElement("Track")]
         public TracksDiscTrack[] Track { get; set; }
 
+        [XmlAttribute()]
         public byte Number { get; set; }
     }
 
     public class TracksDiscTrack
     {
+        [XmlAttribute()]
         public byte Number { get; set; }
 
+        [XmlText()]
         public string Value { get; set; }
     }
 }
