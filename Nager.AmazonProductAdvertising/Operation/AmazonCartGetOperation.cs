18aae525c3d05805e8a25e2b11d1d1218fad369d
Mohammed Wed <mohammedwed@hotmail.com>
2018-01-28 14:28:55 +0200

    Cart Add, Get, Clear operations

---
+++ Nager.AmazonProductAdvertising/Operation/AmazonCartGetOperation.cs
@@ -0,0 +1,23 @@
+
+using Nager.AmazonProductAdvertising.Model;
+
+namespace  Nager.AmazonProductAdvertising.Operation
+{
+    public class  AmazonCartGetOperation : AmazonOperationBase
+    {
+
+        public AmazonCartGetOperation()
+        {
+            base.ParameterDictionary.Add("Operation", "CartGet");
+        }
+
+        public void GetCart(Cart cart)
+        {
+            base.ParameterDictionary.Add("CartId", cart.CartId);
+            base.ParameterDictionary.Add("HMAC", cart.HMAC);
+        }
+
+
+
+    }
+}
