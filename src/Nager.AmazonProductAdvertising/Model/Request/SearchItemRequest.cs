aff57dab0992123a1082297c2cea928aa1629e81
Christoph Wagner <christoph@cwagner.me>
2019-11-08 10:08:37 +0100

    Added Brand and Title for the SearchItems endpoint

--- src/Nager.AmazonProductAdvertising/Model/Request/SearchItemRequest.cs
+++ src/Nager.AmazonProductAdvertising/Model/Request/SearchItemRequest.cs
@@ -6,6 +6,8 @@ namespace Nager.AmazonProductAdvertising.Model.Request
     public class SearchItemRequest : AmazonRequest
     {
         public string Keywords { get; set; }
+        public string Brand { get; set; }
+        public string Title { get; set; }
         public int? ItemPage { get; set; }
         [JsonConverter(typeof(StringEnumConverter))]
         public SortBy? SortBy { get; set; }
