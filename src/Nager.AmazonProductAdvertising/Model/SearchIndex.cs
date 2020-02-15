6dffa378f5084dd2f7ab9193e5d3c60dd448de30
Joseph Petti <petti.joseph@gmail.com>
2020-02-14 20:49:17 -0500

    Added SearchIndex integration for allowing to narrow down searches.

---
+++ src/Nager.AmazonProductAdvertising/Model/SearchIndex.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nager.AmazonProductAdvertising.Model
+{
+    /// <summary>
+    /// Search Indexes, more information located https://docs.aws.amazon.com/AWSECommerceService/latest/DG/localevalues.html
+    /// </summary>
+    public enum SearchIndex
+    {
+        All,
+        Apparel,
+        Automotive,
+        Baby,
+        Beauty,
+        Blended,
+        Books,
+        Classical,
+        DigitalMusic,
+        DVD,
+        Electronics,
+        ForeignBooks,
+        GourmetFood,
+        Grocery,
+        HealthPersonalCare,
+        Hobbies,
+        HomeGarden,
+        Industrial,
+        Jewelry,
+        KindleStore,
+        Kitchen,
+        Magazines,
+        Merchants,
+        Miscellaneous,
+        MP3Downloads,
+        Music,
+        MusicalInstruments,
+        MusicTracks,
+        OfficeProducts,
+        OutdoorLiving,
+        PCHardware,
+        PetSupplies,
+        Photo,
+        Software,
+        SoftwareVideoGames,
+        SportingGoods,
+        Tools,
+        Toys,
+        VHS,
+        Video,
+        VideoGames,
+        Watches,
+        Wireless,
+        WirelessAccessories,
+    }
+}
