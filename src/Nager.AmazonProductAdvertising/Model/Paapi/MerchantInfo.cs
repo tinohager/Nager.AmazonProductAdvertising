833869b9bb88ead1dd22facb5f854826038c0180
Tino Hager <tino.hager@nager.at>
2020-11-06 20:58:42 +0100

    Update MerchantInfo

--- src/Nager.AmazonProductAdvertising/Model/Paapi/MerchantInfo.cs
+++ src/Nager.AmazonProductAdvertising/Model/Paapi/MerchantInfo.cs
@@ -5,5 +5,7 @@
         public string Id { get; set; }
         public string Name { get; set; }
         public string DefaultShippingCountry { get; set; }
+        public int FeedbackCount { get; set; }
+        public double FeedbackRating { get; set; }
     }
 }
